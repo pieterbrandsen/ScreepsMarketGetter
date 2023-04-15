392715d1a1cd1237fd2a85f6d151de64e12f8dbd
Pieter Brandsen <pieterbrandsen2@gmail.com>
2023-04-15 22:14:23 +0200

    Added log file export

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.get('/', (req, res) => {
   const lastDailyFile = fs.readdirSync('./dailyFiles').sort().reverse()[0];
   const lastCompleteFile = fs.readdirSync('./completeFiles').sort().reverse()[0];
   const now = new Date();
-  
+
   const diffDailyDays = Math.ceil(Math.abs(parseInt(now.getTime()) - parseInt(lastDailyFile)) / (1000 * 60 * 60 * 24));
   const diffCompleteMinutes = Math.ceil(Math.abs(parseInt(now.getTime()) - parseInt(lastCompleteFile)) / (1000 * 60));
 
   const online = diffDailyDays < 10 && diffCompleteMinutes < 600;
-  res.send({result: online, lastDailyFile, lastCompleteFile, diffDailyDays, diffCompleteMinutes})
+  res.send({ result: online, lastDailyFile, lastCompleteFile, diffDailyDays, diffCompleteMinutes })
 })
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
@@ -33,7 +33,7 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "log.log" }),
+    new winston.transports.File({ filename: "logs/log.log" }),
   ],
 });
 
@@ -79,7 +79,7 @@ const logger = winston.createLogger({
       const dailyMarketData = event.data.messages.log.find((message) =>
         message.startsWith('[{"resourceType')
       );
-      if (dailyMarketData)
+      if (dailyMarketData) {
         dailyShardsData = dailyData(
           data.shard,
           dailyShardsData,
@@ -87,6 +87,8 @@ const logger = winston.createLogger({
           dailyMarketData,
           currentTime
         );
+        logger.info(`Daily data for shard ${data.shard} received`);
+      }
     });
   } catch (error) {
     logger.error(error);
