6116af171a6312b8f88f6f4a641eeb71aadffc0d
Pieter Brandsen <pieterbrandsen2@gmail.com>
2023-11-23 21:11:15 +0100

    bUG

--- src/index.js
+++ src/index.js
@@ -12,97 +12,97 @@ const app = express()
 const port = process.env.PORT
 
 const emptyShardsData = {
-  shard0: [],
-  shard1: [],
-  shard2: [],
-  shard3: [],
+    shard0: [],
+    shard1: [],
+    shard2: [],
+    shard3: [],
 };
 
 let detailedShardsData = {}
 let dailyShardsData = {}
 
 app.get('/', (req, res) => {
-  const lastDailyFile = fs.readdirSync('./dailyFiles').sort().reverse()[0];
-  const lastCompleteFile = fs.readdirSync('./completeFiles').sort().reverse()[0];
-  const now = new Date();
+    const lastDailyFile = fs.readdirSync('./dailyFiles').sort().reverse()[0];
+    const lastCompleteFile = fs.readdirSync('./completeFiles').sort().reverse()[0];
+    const now = new Date();
 
-  const diffDailyDays = Math.ceil(Math.abs(parseInt(now.getTime()) - parseInt(lastDailyFile)) / (1000 * 60 * 60 * 24));
-  const diffCompleteMinutes = Math.ceil(Math.abs(parseInt(now.getTime()) - parseInt(lastCompleteFile)) / (1000 * 60));
+    const diffDailyDays = Math.ceil(Math.abs(parseInt(now.getTime()) - parseInt(lastDailyFile)) / (1000 * 60 * 60 * 24));
+    const diffCompleteMinutes = Math.ceil(Math.abs(parseInt(now.getTime()) - parseInt(lastCompleteFile)) / (1000 * 60));
 
-  const online = diffDailyDays < 10 && diffCompleteMinutes < 600;
-  const statusDetailedShards = {}
-  const statusDailyShards = {};
-  const shardNames = Object.keys(emptyShardsData);
-  for (let s = 0; s < shardNames.length; s += 1) {
-    const shardName = shardNames[s];
-    statusDailyShards[shardName] = dailyShardsData[shardName].length;
-    statusDetailedShards[shardName] = detailedShardsData[shardName].length;
-  }
-  res.send({ result: online, lastDailyFile, lastCompleteFile, diffDailyDays, diffCompleteMinutes, detailedShardsData: statusDetailedShards, dailyShardsData: statusDailyShards })
+    const online = diffDailyDays < 10 && diffCompleteMinutes < 600;
+    const statusDetailedShards = {}
+    const statusDailyShards = {};
+    const shardNames = Object.keys(emptyShardsData);
+    for (let s = 0; s < shardNames.length; s += 1) {
+        const shardName = shardNames[s];
+        statusDailyShards[shardName] = dailyShardsData[shardName] ? dailyShardsData[shardName].length : undefined;
+        statusDetailedShards[shardName] = detailedShardsData[shardName] ? detailedShardsData[shardName].length : undefined;
+    }
+    res.send({ result: online, lastDailyFile, lastCompleteFile, diffDailyDays, diffCompleteMinutes, detailedShardsData: statusDetailedShards, dailyShardsData: statusDailyShards })
 })
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
 
 const logger = createLogger({
-  format: format.combine(
-    format.timestamp(),
-    format.json()
-  ),
-  transports: [
-    new transports.DailyRotateFile({
-      filename: `logs/application-%DATE%.log`,
-      auditFile: `logs/audit.json`,
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d'
-    })
-  ],
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
+    transports: [
+        new transports.DailyRotateFile({
+            filename: `logs/application-%DATE%.log`,
+            auditFile: `logs/audit.json`,
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d'
+        })
+    ],
 });
 
 (async function () {
-  try {
-    const api = new ScreepsAPI();
-    await api.auth(process.env.SCREEPS_USERNAME, process.env.SCREEPS_PASSWORD);
-    await api.socket.connect();
+    try {
+        const api = new ScreepsAPI();
+        await api.auth(process.env.SCREEPS_USERNAME, process.env.SCREEPS_PASSWORD);
+        await api.socket.connect();
 
-    detailedShardsData = _.clone(emptyShardsData);
-    dailyShardsData = _.clone(emptyShardsData);
-    api.socket.subscribe("console", (event) => {
-      const data = event.data;
-      const currentTime = Date.now();
-      if (!data || !event.data.messages || event.data.messages.log.length === 0)
-        return;
+        detailedShardsData = _.clone(emptyShardsData);
+        dailyShardsData = _.clone(emptyShardsData);
+        api.socket.subscribe("console", (event) => {
+            const data = event.data;
+            const currentTime = Date.now();
+            if (!data || !event.data.messages || event.data.messages.log.length === 0)
+                return;
 
-      const detailedMarketData = event.data.messages.log.find((message) =>
-        message.startsWith('[{"created')
-      );
-      if (detailedMarketData) {
-        detailedShardsData = detailedData(
-          data.shard,
-          detailedShardsData,
-          emptyShardsData,
-          detailedMarketData,
-          currentTime
-        );
-        logger.info(`Detailed data for shard ${data.shard} received`);
-      }
+            const detailedMarketData = event.data.messages.log.find((message) =>
+                message.startsWith('[{"created')
+            );
+            if (detailedMarketData) {
+                detailedShardsData = detailedData(
+                    data.shard,
+                    detailedShardsData,
+                    emptyShardsData,
+                    detailedMarketData,
+                    currentTime
+                );
+                logger.info(`Detailed data for shard ${data.shard} received`);
+            }
 
-      const dailyMarketData = event.data.messages.log.find((message) =>
-        message.startsWith('[{"resourceType')
-      );
-      if (dailyMarketData) {
-        dailyShardsData = dailyData(
-          data.shard,
-          dailyShardsData,
-          emptyShardsData,
-          dailyMarketData,
-        );
-        logger.info(`Daily data for shard ${data.shard} received`);
-      }
-    });
-  } catch (error) {
-    logger.error(error);
-  }
+            const dailyMarketData = event.data.messages.log.find((message) =>
+                message.startsWith('[{"resourceType')
+            );
+            if (dailyMarketData) {
+                dailyShardsData = dailyData(
+                    data.shard,
+                    dailyShardsData,
+                    emptyShardsData,
+                    dailyMarketData,
+                );
+                logger.info(`Daily data for shard ${data.shard} received`);
+            }
+        });
+    } catch (error) {
+        logger.error(error);
+    }
 })();
